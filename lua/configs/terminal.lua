006ad2b39283948f44f703d51f34f74b6608f6c5
Evan McPheron <evaninja2@gmail.com>
2024-03-19 22:08:12 -0500

    Fixed Lazygit issue
    Lazygit cmd was opening with line numbers on side, wrote code to prevent that.

--- lua/configs/terminal.lua
+++ lua/configs/terminal.lua
@@ -12,12 +12,19 @@ require('terminal').setup {
 
 local lazygit = require("terminal").terminal:new({
     layout = { open_cmd = "float", height = 0.9, width = 0.9 },
-    cmd = { "lazygit" },
+    cmd = { 
+      "lazygit"
+    },
     autoclose = true,
 })
 vim.env["GIT_EDITOR"] = "nvr -cc close -cc split --remote-wait +'set bufhidden=wipe'"
 vim.api.nvim_create_user_command("Lazygit", function(args)
     lazygit.cwd = args.args and vim.fn.expand(args.args)
     lazygit:toggle(nil, true)
+    if next(lazygit:get_current_tab_windows()) then
+      vim.cmd("set nonumber")
+      vim.cmd("set norelativenumber")
+    end
+    
 end, { nargs = "?" })
 
