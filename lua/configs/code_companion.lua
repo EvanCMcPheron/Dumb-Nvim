8e44e3d330df2f6bd4f5b051df3fe195a7cc6df4
Evan McPheron <evaninja2@gmail.com>
2024-07-25 17:15:16 -0500

    AI mappings:CodeCompanion

--- lua/configs/code_companion.lua
+++ lua/configs/code_companion.lua
@@ -1,9 +1,21 @@
+local adapter = "openai"
+
 return function()
   vim.env.ANTHROPIC_API_KEY = require('api_keys').anthropic
+  vim.env.OPENAI_API_KEY = require('api_keys').open_ai
   require("codecompanion").setup({
     adapters = {
+      openai = function()
+        return require("codecompanion.adapters").use("openai", {
+          schema = {
+            model = {
+              default = "gpt-4o-mini",
+            },
+          },
+        })
+      end,
       anthropic = function()
-        return require("codecompanion.adapters").use("anthropic", {
+        return require("codecompanion.adapters").use(adapter, {
           schema = {
             model = {
               default = "claude-3-5-sonnet-20240620",
@@ -14,13 +26,13 @@ return function()
     },
     strategies = {
       chat = {
-        adapter = "anthropic",
+        adapter = adapter,
       },
       inline = {
-        adapter = "anthropic",
+        adapter = adapter,
       },
       agent = {
-        adapter = "anthropic"
+        adapter = adapter
       },
     },
   })
